fa5cd5c7fd986505936400ded65829536a13a30b
chaituRam77 <chaitu4tech@gmail.com>
2023-03-30 22:27:21 +0200

    working version

--- src/components/AuctionPlayer.vue
+++ src/components/AuctionPlayer.vue
@@ -1,165 +1,23 @@
-<template>
-  <div>
-    <div class="d-flex justify-content-between align-items-center my-3">
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="basePriceDropdown"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Base Price: {{ basePrice }}
-        </button>
-        <div class="dropdown-menu" aria-labelledby="basePriceDropdown">
-          <a class="dropdown-item" href="#" @click="setBasePrice(1)">1</a>
-          <a class="dropdown-item" href="#" @click="setBasePrice(3)">3</a>
-          <a class="dropdown-item" href="#" @click="setBasePrice(5)">5</a>
-          <a class="dropdown-item" href="#" @click="setBasePrice(10)">10</a>
-        </div>
-      </div>
-
-      <div class="dropdown">
-        <button
-          class="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="categoryDropdown"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          Category: {{ category }}
-        </button>
-        <div class="dropdown-menu" aria-labelledby="categoryDropdown">
-          <a class="dropdown-item" href="#" @click="setCategory('batsman')"
-            >Batsman</a
-          >
-          <a class="dropdown-item" href="#" @click="setCategory('bowler')"
-            >Bowler</a
-          >
-          <a class="dropdown-item" href="#" @click="setCategory('wicketkeeper')"
-            >Wicketkeeper</a
-          >
-          <a class="dropdown-item" href="#" @click="setCategory('allrounder')"
-            >Allrounder</a
-          >
-        </div>
-      </div>
-
-      <button class="btn btn-primary" @click.prevent="callAPI2">
-        Call API
-      </button>
-    </div>
-
-    <div v-if="playerData">
-      <h3>{{ playerData.name }}</h3>
-      <p>Team: {{ playerData.team }}</p>
-      <p>Country: {{ playerData.country }}</p>
-      <img :src="playerData.picture" alt="Player Picture" />
-    </div>
-  </div>
-</template>
-
+<<vue-splitter-v :left-width="300" :right-min-width="200">
+  <div slot="left">Left panel content</div>
+  <div slot="right">Right panel content</div>
+</vue-splitter-v>
+>
 <script>
-import axios from "axios";
+import { MultiSplitPane, Pane } from 'vue-multi-split-pane';
 export default {
-  data() {
-    return {
-      basePrice: 3,
-      category: "batsman",
-      playerData: null,
-    };
-  },
-  methods: {
-    setBasePrice(price) {
-      this.basePrice = price;
-    },
-    setCategory(category) {
-      this.category = category;
-    },
-    callBackEnd() {
-      axios.get("https://teatapp.onrender.com/getRandomPlayer?basePrice=3&category=batsman").then((response) => {
-          this.apiScore = response.data;
-          console.log(this.apiScore);
-        });
-    },
-    callAPI() {
-      const apiUrl = "http://localhost:8080/getRandomPlayer";
-      const queryParams = {
-        basePrice: this.basePrice,
-        category: this.category,
-      };
-      const url = new URL(apiUrl);
-      Object.keys(queryParams).forEach((key) =>
-        url.searchParams.append(key, queryParams[key])
-      );
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          this.playerData = data;
-          console.log(data);
-        })
-        .catch((error) => console.error(error));
-    },
-    async getAllUsers() {
-      const response = await fetch("getOwnersTeam", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
-      console.log("RESPONSE..." + response);
-      return await response.json();
-    },
-
-    async createUser(data) {
-      const response = await fetch(`/api/user`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ user: data }),
-      });
-      return await response.json();
-    },
-
-    async callAPI2() {
-      console.log("Calliing");
-      const options = {
-        method: "GET",
-        url: "https://teatapp.onrender.com/getRandomPlayer",
-        // url: "http://localhost:8080/getRandomPlayer",
-        // url: "getRandomPlayer",
-        params: { basePrice: this.basePrice, category: this.category },
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
-      };
-      await axios
-        .request(options)
-        .then((response) => {
-          this.apiScore = response.data;
-          console.log(this.apiScore);
-        })
-        .catch((error) => {
-          console.log("ERORR FACEED");
-          console.log(error);
-        });
-    },
-  },
+  components: {
+    MultiSplitPane,
+    Pane
+    // ...
+  }
 };
 </script>
 
-<style scoped>
-.dropdown-toggle::after {
-  display: none;
-}
-
-.dropdown-menu {
-  box-shadow: none;
-}
-
-.dropdown-item.active,
-.dropdown-item:active {
-  background-color: #007bff;
-  color: #fff;
+<!-- optionally use our default style -->
+<style>
+/* @import 'vue-split-pane/dist/vue-split-pane.esm.css'; */
+.vue-splitter-v {
+  height: 100%;
 }
 </style>
